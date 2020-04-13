7bdf84e7d58bbcae4e17b07dabd2377263626eb2
chachachacharlirae <61812759+chachachacharlirae@users.noreply.github.com>
2020-04-12 22:49:36 -0500

    Add files via upload

---
+++ quizq.js
@@ -0,0 +1,49 @@
+var questions = [
+    {
+        title: "What is a function?",
+        choices: ["a predefined action", "an expresion", "basic syntax", "java"],
+        answer: "a predefined action"
+    },
+    {
+        title: "What closes out an alert function's code?",
+        choices: [".", ";", ":", "!"],
+        answer: ";"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Where is a period placed in dot notation.",
+        choices: ["before the object name", "at the end", "between the object name and property", "none of the above"],
+        answer: "between the object name and property"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "What does API stand for?",
+        choices: ["web application programming interface", "Area per square inch", "Aspect percentage inspection", "None of the Above"],
+        answer: "web application programming interface"
+    },
+    {
+        title: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "What is a variable?",
+        choices: ["basic syntax", "a symbol", "{}", "a named location for a value"],
+        answer: "a named location for a value"
+    },
+    {
+        title: "Which text displays camel casing?",
+        choices: ["runbuddY", "runBuddy", "RunBuddY", "Runbuddy"],
+        answer: "runBuddy"
+    }
+];
+
+var userScores = []
